2e9570b6cd71757fdd2666bdee81dc452c059487
gaowei <gwblue@163.com>
2016-09-04 22:51:45 +0800

    Add nim sdk 2.8

--- weishu/src/main/java/com/garfield/weishu/application/MyApplication.java
+++ weishu/src/main/java/com/garfield/weishu/application/MyApplication.java
@@ -2,9 +2,7 @@ package com.garfield.weishu.application;
 
 import android.app.Application;
 
-import com.garfield.weishu.config.AppCache;
-import com.garfield.weishu.sdk.nim.NimHelper;
-import com.netease.nimlib.sdk.NIMClient;
+
 
 /**
  * Created by gaowei3 on 2016/8/30.
@@ -14,10 +12,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        AppCache.setContext(this);
 
-        //init以外的所有方法，都只能在UI进程中调用
-        NIMClient.init(this, NimHelper.loginInfo(), NimHelper.options(this));
+
 
 
     }
--- weishu/src/main/java/com/garfield/weishu/sdk/nim/Register.java
+++
@@ -1,9 +0,0 @@
-package com.garfield.weishu.sdk.nim;
-
-/**
- * Created by gaowei3 on 2016/8/30.
- */
-public class Register {
-
-
-}
