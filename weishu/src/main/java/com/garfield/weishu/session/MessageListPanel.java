0757b4afaa377dfca7477340b69946eb2ebe7863
gaowei <gwblue@163.com>
2016-10-08 14:14:15 +0800

    10.8

--- weishu/src/main/java/com/garfield/weishu/session/MessageListPanel.java
+++ weishu/src/main/java/com/garfield/weishu/session/MessageListPanel.java
@@ -4,9 +4,12 @@ import android.view.View;
 
 import com.garfield.weishu.base.adapter.TAdapterDelegate;
 import com.garfield.weishu.base.adapter.TViewHolder;
+import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum;
 import com.netease.nimlib.sdk.msg.model.IMMessage;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -20,6 +23,9 @@ public class MessageListPanel implements TAdapterDelegate {
     private MsgAdapter adapter;
     private View rootView;
 
+    private SessionTypeEnum sessionTypeEnum = SessionTypeEnum.P2P;
+    private String account;
+
     public MessageListPanel(View rootView) {
         ButterKnife.bind(this, rootView);
         init();
@@ -31,11 +37,27 @@ public class MessageListPanel implements TAdapterDelegate {
     }
 
     public void onIncomingMessage(List<IMMessage> messages) {
-
+        List<IMMessage> addedListItems = new ArrayList<>(messages.size());
+        boolean needRefresh = false;
+        for (IMMessage message : messages) {
+            if (isMyMessage(message)) {
+                items.add(message);
+                addedListItems.add(message);
+                needRefresh = true;
+            }
+        }
+        if (needRefresh) {
+            sortMessages(items);
+            adapter.notifyDataSetChanged();
+        }
 
     }
 
-
+    public boolean isMyMessage(IMMessage message) {
+        return message.getSessionType() == sessionTypeEnum
+                && message.getSessionId() != null
+                && message.getSessionId().equals(account);
+    }
 
     @Override
     public int getViewTypeCount() {
@@ -51,4 +73,23 @@ public class MessageListPanel implements TAdapterDelegate {
     public boolean enabled(int position) {
         return false;
     }
+
+    /**
+     * **************************** 排序 ***********************************
+     */
+    private void sortMessages(List<IMMessage> list) {
+        if (list.size() == 0) {
+            return;
+        }
+        Collections.sort(list, comp);
+    }
+
+    private static Comparator<IMMessage> comp = new Comparator<IMMessage>() {
+
+        @Override
+        public int compare(IMMessage o1, IMMessage o2) {
+            long time = o1.getTime() - o2.getTime();
+            return time == 0 ? 0 : (time < 0 ? -1 : 1);
+        }
+    };
 }
